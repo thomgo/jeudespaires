e32e219d2d7e5c28c3a80c9fd780c3e583ba1483
Thomas <tg.59290@gmail.com>
2018-04-30 12:09:35 +0200

    game fully functionnal with basic division into functions

--- js/main.js
+++ js/main.js
@@ -1,9 +1,25 @@
 //
 //
-// Functions
+// ~~~~~ Global variables ~~~~~~
 //
 //
 
+var userAnswers = [];
+var clickedCards = [];
+var allowClick = true;
+var score = 0;
+
+//
+//
+// ~~~~~~~ Functions ~~~~~~~~
+//
+//
+
+//
+// DOM Functions
+//
+
+//Create an array of 12 colors and sorts it randomly
 function randomizeColors() {
   var colors = [
     "red",
@@ -24,6 +40,7 @@ function randomizeColors() {
   });
 }
 
+//Create a card element to be displayed on the screen
 function createCard() {
   var card = document.createElement('div');
   card.className = "col-sm-2";
@@ -31,6 +48,7 @@ function createCard() {
   return card;
 }
 
+//Create 12 card elements on the screen
 function addCards() {
   var boardGame = document.getElementById("boardGame");
   for (var i = 0; i < 12; i++) {
@@ -39,63 +57,105 @@ function addCards() {
   }
 }
 
+//Change a card color
 function changeCardColor(card, color) {
   card.style.background = color;
 }
 
+//
+//Game calculation functions
+//
 
-function addClickOnCards(colors, score) {
-    var isWaiting = false;
-    var userAnswers = [];
-    var clickedCards = [];
-    var cards = document.getElementsByClassName("card");
-    for (let i = 0; i < cards.length; i++) {
-      cards[i].addEventListener("click", function(){
-        if(!isWaiting) {
-          changeCardColor(this, colors[i]);
-
-          if(userAnswers.length < 2 && clickedCards.length < 2) {
-            userAnswers.push(colors[i]);
-            clickedCards.push(this);
-
-            if(userAnswers.length === 2 && clickedCards.length === 2) {
-              if(userAnswers[0] !== userAnswers[1]) {
-                setTimeout(function(){
-                  changeCardColor(clickedCards[0], "black");
-                  changeCardColor(clickedCards[1], "black");
-                  userAnswers = [];
-                  clickedCards = [];
-                  isWaiting = false;
-                }, 1500);
-                isWaiting = true;
-              }
-              else {
-                score ++;
-                if(score === 6) {
-                  alert("vous avez gagné !");
-                  location.reload();
-                }
-                userAnswers = [];
-                clickedCards = [];
-              }
-
-            }
-          }
-        }
-      });
+//Store the selected color and the card element in arrays
+function registerAnswers(userAnswer, clickedCard) {
+  if(userAnswers.length < 2 && clickedCards.length < 2) {
+    userAnswers.push(userAnswer);
+    clickedCards.push(clickedCard);
   }
 }
 
-function startGame() {
-  addCards();
-  var colors = randomizeColors();
-  var score = 0;
-  addClickOnCards(colors, score);
+//Check if the the user answer is correct
+function checkAnswers() {
+  if(userAnswers[0] !== userAnswers[1]) {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+//Turn the selected cards into black cards
+function clearCards() {
+  setTimeout(function(){
+    changeCardColor(clickedCards[0], "black");
+    changeCardColor(clickedCards[1], "black");
+  }, 1500);
+}
+
+//Clears the arrays containing the user answers and the clicked cards
+function clearAnswers(madePoint = false) {
+  if(!madePoint){
+    setTimeout(function(){
+      userAnswers = [];
+      clickedCards = [];
+      allowClick = true;
+    }, 1600);
+  }
+  else  {
+    userAnswers = [];
+    clickedCards = [];
+    allowClick = true;
+  }
+}
+
+//Add a point to the user score and restart the game if the user has won
+function addPoint() {
+  score ++;
+  if(score === 6) {
+    restartGame();
+  }
+}
+
+//Restart the game
+function restartGame() {
+  alert("vous avez gagné !");
+  location.reload();
 }
 
 //
 //
-// Code logic
+// ~~~~~~ Code logic ~~~~~~~~
 //
 //
+
+//Initialize the game
+function startGame() {
+  addCards();
+
+  var colors = randomizeColors();
+  var cards = document.getElementsByClassName("card");
+
+  for (let i = 0; i < cards.length; i++) {
+    cards[i].addEventListener("click", function(){
+      if(allowClick) {
+        changeCardColor(this, colors[i]);
+        registerAnswers(colors[i], this);
+            if(userAnswers.length === 2 && clickedCards.length === 2) {
+              allowClick = false;
+              if(checkAnswers()){
+                addPoint();
+                clearAnswers(true);
+              }
+              else {
+                clearCards();
+                clearAnswers();
+              }
+            }
+      }
+    });
+  }
+
+}
+
+
 startGame();
